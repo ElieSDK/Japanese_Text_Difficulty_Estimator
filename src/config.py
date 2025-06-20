f0bd6ba30f3857e446fcf4e0f0a4d10583a96721
ElieSDK <66425607+ElieSDK@users.noreply.github.com>
2025-06-20 10:05:49 +0200

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# PATH to chromedriver folder
+CHROMEDRIVER_PATH = "C:/Users/PC/Downloads/chromedriver-win64/chromedriver.exe"
+
+# Poppler path for PDF to image conversion
+POPPLER_PATH = r"C:\poppler\Library\bin"
+
+# Tesseract path for OCR
+TESSERACT_CMD = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
