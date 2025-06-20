da6e01d7ffbc4667958e8e91456d2a13f283c0a2
ElieSDK <66425607+ElieSDK@users.noreply.github.com>
2025-06-20 17:29:37 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,14 +6,16 @@ from pathlib import Path
 
 @st.cache_resource
 def load_model():
-    # Get the path of the running script
     try:
         current_dir = Path(__file__).parent
     except NameError:
-        # Fallback if __file__ is not defined (e.g. in Streamlit)
         current_dir = Path(sys.argv[0]).parent.resolve()
-    
     model_path = current_dir / "logreg_pipeline.pkl"
-    return joblib.load(model_path)
+    
+    if not model_path.exists():
+        raise FileNotFoundError(f"Model file not found: {model_path}")
+    
+    model = joblib.load(model_path)
+    return model
 
 model = load_model()
