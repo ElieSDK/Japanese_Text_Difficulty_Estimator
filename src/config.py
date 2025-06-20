f8d64c8817dea319f921fa30dd5e8d9f356ab46c
ElieSDK <66425607+ElieSDK@users.noreply.github.com>
2025-06-20 16:24:18 +0200

    Delete src directory

--- src/config.py
+++
@@ -1,8 +0,0 @@
-# PATH to chromedriver folder
-CHROMEDRIVER_PATH = "C:/Users/PC/Downloads/chromedriver-win64/chromedriver.exe"
-
-# Poppler path for PDF to image conversion
-POPPLER_PATH = r"C:\poppler\Library\bin"
-
-# Tesseract path for OCR
-TESSERACT_CMD = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
